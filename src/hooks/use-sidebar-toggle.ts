d8f8459bb6e015b4544b5665735d1550fb8f6407
Johan Guse <johanguse@gmail.com>
2024-01-17 17:48:12 -0300

    test: remove dashboard admin

--- src/hooks/use-sidebar-toggle.ts
+++ src/hooks/use-sidebar-toggle.ts
@@ -1,11 +1,17 @@
-import { create } from "zustand"
+import create from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
 interface SidebarToggle {
-    toggleCollapse: boolean,
-    invokeToggleCollapse: () => void
+  toggleCollapse: boolean
+  invokeToggleCollapse: () => void
 }
 
-export const useSideBarToggle = create<SidebarToggle>((set, get) => ({
+export const useSideBarToggle = create(
+  immer<SidebarToggle>((set) => ({
     toggleCollapse: false,
-    invokeToggleCollapse: () => set({ toggleCollapse: !get().toggleCollapse })
-}))
+    invokeToggleCollapse: () =>
+      set((state) => {
+        state.toggleCollapse = !state.toggleCollapse
+      }),
+  }))
+)
