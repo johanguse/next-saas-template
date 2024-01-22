cc7e0bc02befe8400474b2768bc0ae12c3cd493a
Johan Guse <johanguse@gmail.com>
2024-01-22 16:24:58 -0300

    feat: create privacy and cookie policy page

---
+++ src/app/(marketing)/cookies-policy/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Cookies Policy',
+}
+
+export default function CookiePolicyPage() {
+  return (
+    <div className="flex w-full flex-col gap-16 py-8 md:py-8">
+      <div className="container">
+        <div className="mx-auto flex max-w-xl justify-center"></div>
+      </div>
+    </div>
+  )
+}
