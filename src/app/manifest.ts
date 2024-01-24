a34ae9de5c01c9784cc9a01b38f45a0fe65aaf81
Johan Guse <johanguse@gmail.com>
2024-01-24 20:53:22 -0300

    feat: improve the seo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,19 +1,52 @@
 import { MetadataRoute } from 'next'
 
+import { siteConfig } from '@/config/site'
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
+    name: siteConfig.name,
+    short_name:
+      'Empowering your SaaS startup journey with a meticulously crafted Next.js boilerplate',
+    description: siteConfig.description,
     start_url: '/',
     display: 'standalone',
-    background_color: '#fff',
+    background_color: '#c423e7',
     theme_color: '#fff',
     icons: [
       {
         src: 'favicons/favicon.ico',
         sizes: 'any',
         type: 'image/x-icon',
+      },
+      {
+        src: 'favicons/android-icon-192x192.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+      {
+        src: 'favicons/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: 'favicons/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: 'favicons/android-icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: 'favicons/android-icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: 'favicons/android-icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
     ],
   }
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,17 @@
 import { MetadataRoute } from 'next'
 
+import { siteConfig } from '@/config/site'
+
 export default function robots(): MetadataRoute.Robots {
+  const siteUrl = new URL(siteConfig.url)
+
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/api', '/dashboard', '/dashboard-admin'],
     },
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: `${siteUrl}`,
   }
 }
