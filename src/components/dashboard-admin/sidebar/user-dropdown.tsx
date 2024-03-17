9128a39a99985b04524afc194c81115e8fcb4be7
Johan Guse <johanguse@gmail.com>
2024-03-17 15:35:22 -0300

    chore: improve dashboard user

--- src/components/dashboard-admin/sidebar/user-dropdown.tsx
+++ src/components/dashboard-admin/sidebar/user-dropdown.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -10,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
-import { LogOut, RocketIcon, Settings2 } from 'lucide-react'
+import { LogOut, RocketIcon, Settings, Settings2 } from 'lucide-react'
 import { Session } from 'next-auth'
 import { signOut } from 'next-auth/react'
 
@@ -21,12 +23,14 @@ type UserDropdownProps = {
 export function UserDropdown({ user }: UserDropdownProps) {
   if (!user) return
 
+  const userRole = user.role
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="link"
-          className="relative flex h-8 w-full items-center justify-between space-x-2 !px-0"
+          className="relative flex h-8 w-full cursor-pointer items-center justify-between space-x-2 !px-0 hover:no-underline"
         >
           <Avatar className="size-8">
             <AvatarImage src={user.image as string} alt={user.name as string} />
@@ -43,8 +47,12 @@ export function UserDropdown({ user }: UserDropdownProps) {
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
+      <DropdownMenuContent
+        className="w-56 cursor-pointer"
+        align="end"
+        forceMount
+      >
+        <DropdownMenuLabel className="cursor-pointer font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">{user.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
@@ -54,17 +62,33 @@ export function UserDropdown({ user }: UserDropdownProps) {
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer">
             <Settings2 className="mr-3 size-3" />
             Configuraçoes
           </DropdownMenuItem>
-          <DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer">
             <RocketIcon className="mr-3 size-3" />
             Upgrade
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut()}>
+        {(userRole === 'ADMIN' || userRole === 'EDITOR') && (
+          <>
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild className="cursor-pointer">
+                <Link
+                  href="/dashboard"
+                  className="flex items-center space-x-2.5"
+                >
+                  <Settings className="size-3" />
+                  <p className="text-sm">Regular dashboard</p>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+          </>
+        )}
+        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
           <LogOut className="mr-3 size-3" />
           Log out
         </DropdownMenuItem>
