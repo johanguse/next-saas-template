d99fed3d5cbfa1423505cee8f0978ff35344d25f
Johan Guse <johanguse@gmail.com>
2024-05-31 13:41:12 -0300

    feat: add bento grid

--- next.config.js
+++ next.config.js
@@ -33,6 +33,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
     ],
   },
   reactStrictMode: true,
